9ec6576130e3bcc275eed7a0108d14de6738ee5f
hyelight <zheldeld@gmail.com>
2023-04-05 20:51:06 +0900

    email과 checkbox 유효성 검사

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Community = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const validateEmail = (email) => {
+    const regex =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+    return regex.test(email);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateEmail(email)) {
+      if (document.querySelector("#checkbox").checked) {
+        alert("You have entered a valid email address!");
+      } else {
+        setError("You need to agree to join WORKOUT community.");
+      }
+    } else {
+      setError("You have entered an invalid email address! Try again.");
+    }
+  };
+
   return (
     <section className="bg-[var(--nav-bg)]" id="community">
       <h2 className="sr-only">커뮤니티 합류 여부</h2>
@@ -12,14 +38,19 @@ const Community = () => {
           </span>{" "}
           Community
         </h3>
-        <form className="flex gap-7 justify-center mb-5">
+        <form
+          className="flex gap-7 justify-center mb-5"
+          onSubmit={handleSubmit}
+        >
           <input
             className="rounded-full px-5 text-black"
             type="email"
             placeholder="Enter your email"
+            onChange={handleEmailChange}
           />
           <button>Sign Up</button>
         </form>
+        {error && <p className="text-red-500 mb-3">{error}</p>}
         <form>
           <input type="checkbox" id="checkbox" />
           <label htmlFor="checkbox">I agree to join WORKOUT community.</label>
